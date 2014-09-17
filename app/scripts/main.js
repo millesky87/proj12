9f1ea531062d580bf252ad8504ec09023b5d58e5
Skylar Miller <skyheamillz@gmail.com>
2014-09-17 08:06:02 -0400

    initial commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('The Iron Yard Rocks');
+function issueFunction(container, templateID, model) {
+    var templateFunction = _.template($('#' + templateID).text());
+    var renderTemplate = templateFunction(model);
+    $(container).append(renderTemplate);
+}
+
+$.ajax("https://api.github.com/issues", {
+    type: 'GET',
+    datatype: 'json'
+}).done(function(issues) {
+    _.each(issues, function(issue) {
+    issueFunction('.issues-list', 'template-list', issue);
+    });
+
+});
+
+setInterval (function(){
+	$(document).on('click', '.link', function(e){
+e.preventDefault();
+  $('.stuff').empty();
+    var title = $(this).text();
+	    $.ajax({
+	    type: 'GET',
+	    datatype: 'json',
+	    url: $(this).attr('data-id')
+})
+
+.done(function(issues) {
+	    	console.log(issues);
+
+      _.each(issues, function(issue) {
+	     issue.commentTitle = title;
+	     issueFunction('.context', 'descript', issue);
+        	});
+    	});
+	});
+}, 10000);
